d774de3f99466f69e46e6ad09506ec733042c259
hemanth-kumar-1 <160735662+hemanth-kumar-1@users.noreply.github.com>
2024-03-14 16:17:31 +0530

    Update cryptohack.py

--- cryptohack.py
+++ cryptohack.py
@@ -15,14 +15,14 @@ for i in lst:
 
 #Task 4
 dec = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
-print(bytes.fromhex(dec))
+print(bytes.fromhex(dec))                                    #converets hex code into bytes
 # flag = crypto{You_will_be_working_with_hex_strings_a_lot}
 
 #Task 5
 import base64
 hex = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
 bin = bytes.fromhex(hex)
-b64 = base64.b64encode(bin)
+b64 = base64.b64encode(bin)                        
 print(b64)
 # flag = crypto/Base+64+Encoding+is+Web+Safe/
 
@@ -30,7 +30,7 @@ print(b64)
 decimal = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
 hexadecimal = hex(decimal)[2:]  
 binary = bytes.fromhex(hexadecimal)
-message = binary.decode('utf-8')
+message = binary.decode('utf-8')                #converts into ascii
 print(message)
 # flag = crypto{3nc0d1n6_4ll_7h3_w4y_d0wn}
 
@@ -38,9 +38,9 @@ print(message)
 a = "label"
 b= 13
 for i in a:
-    c = ord(i)
-    e = c ^ b
-    g = chr(e)
+    c = ord(i)                #converts to respective ascii value
+    e = c ^ b                 #xors the value
+    g = chr(e)                #converts into charecter
     print(g,end="")
 # flag = crypto{aloha}
 
